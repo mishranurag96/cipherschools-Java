70f0e9935bcceeb8679513532072420ef24f8cb7
mishranurag96 <mishranurag96@gmail.com>
2020-04-19 20:48:11 +0530

    version 1

---
+++ src/javaapplication4/JavaApplication4.java
@@ -0,0 +1,59 @@
+package javaapplication4;
+
+import java.util.Scanner;
+
+
+class JavaApplication4 {
+
+    public static void main(String[] args) {
+        
+        String re_run;
+        
+        String[] choiceStack = {"rock","paper","scissors"};
+        
+        int randomNum =(int) (3*Math.random());
+        
+        String computerChoice = choiceStack[randomNum];
+        
+        do{
+        System.out.println("Welcome to the game made by CipherSchools");
+        
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Type in rock or paper or scissors");
+        
+        String userChoice = obj.nextLine();
+        
+        if(computerChoice.equals(userChoice)){
+            System.out.println("Tie!");
+        }
+        
+        else if(userChoice.equals("paper") && computerChoice.equals("rock")){
+            System.out.println("You win!");
+        }
+        else if(userChoice.equals("rock") && computerChoice.equals("scissors")){
+            System.out.println("You win!");
+        }
+        else if(userChoice.equals("scissors") && computerChoice.equals("rock")){
+            System.out.println("You loose!");
+        }
+        else if(userChoice.equals("rock") && computerChoice.equals("paper")){
+            System.out.println("You loose!");
+        }
+        else if(userChoice.equals("paper") && computerChoice.equals("scissors")){
+            System.out.println("You loose!");
+        }
+        else if(userChoice.equals("scissors") && computerChoice.equals("paper")){
+            System.out.println("You win!");
+        }
+        
+        else{
+            System.out.println("Please provide a valid input and tell me when you didn't had a mood of playing why did you come here");
+            
+        }
+        System.out.println("Do you want to re run ? Type in y for yes");
+        re_run = obj.nextLine();
+        }while(re_run.equals("y") || re_run.equals("Y"));
+    }
+    
+}
+
